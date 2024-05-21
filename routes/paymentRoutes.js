cdee90b98b05052bbf3a873e9d1b3ea92d017d46
Kazi murtuza <kazimurtuza11@gmail.com>
2024-05-21 11:31:47 +0600

    update

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,11 +1,12 @@
 const express=require('express');
-const { stripePaymentController, stripePaymentSuccess } = require('../controllers/paymentController');
+const { stripePaymentController, stripePaymentSuccess, freeSubscription } = require('../controllers/paymentController');
 
 const route=express.Router();
 // route    GET|POST|UPDATE|DELETE
 route.get('/payment',stripePaymentController)
 route.get('/payment-success',stripePaymentSuccess)
 route.post('/payment-success',stripePaymentSuccess)
+route.get('/payment-free',freeSubscription)
 
 // export
 module.exports=route
