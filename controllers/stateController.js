b6b4e67110fd8c3e7f3a2a895ca5a5fe1257f127
Sajeeb Chakraborty <sajeebchakraborty.cse2000@gmail.com>
2024-06-18 08:11:32 +0600

    sss

--- controllers/stateController.js
+++ controllers/stateController.js
@@ -4,80 +4,101 @@ const { AuthUser } = require("../utils/helper");
 
 // Define countryController methods
 const stateController = {
-    // Method to create a new state
-    create: async (req, res) => {
-        const { name,country } = req.body;
-        try {
-            const stateInfo = await stateModel.create({ name,country });
-            res.status(201).send({
-                success: true,
-                message: "State Created Successfully",
-                stateInfo
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({
-                success: false,
-                message: 'Error in creating state',
-                error: error.message
-            });
-        }
-    },
+  // Method to create a new state
+  create: async (req, res) => {
+    const { name, country } = req.body;
+    try {
+      const stateInfo = await stateModel.create({ name, country });
+      res.status(201).send({
+        success: true,
+        message: "State Created Successfully",
+        stateInfo,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Error in creating state",
+        error: error.message,
+      });
+    }
+  },
 
-    // Method to list all states
-    list: async (req, res) => {
-        const info = new URL(req.url, `http://${req.headers.host}`);
-        const searchParams = info.searchParams;
-        let page = Number(searchParams.get('page')) || 1;
-        let limit = Number(searchParams.get('limit')) || 12;
-        let skip = (page - 1) * limit;
+  // Method to list all states
+  list: async (req, res) => {
+    const info = new URL(req.url, `http://${req.headers.host}`);
+    const searchParams = info.searchParams;
+    let page = Number(searchParams.get("page")) || 1;
+    let limit = Number(searchParams.get("limit"));
+    let skip = (page - 1) * limit;
 
-        try {
-            const count = await stateModel.countDocuments();            
-            const totalPages = Math.ceil(count / limit);
+    try {
+      const count = await stateModel.countDocuments();
+      const totalPages = Math.ceil(count / limit);
+      let states;
+      if (limit == null || limit == undefined || limit == "") {
+        states = await stateModel
+          .find()
+          .populate([
+            {
+              path: "country",
+              model: "Country",
+            },
+          ])
+          .sort({ createdAt: -1 });
+      } else {
+        states = await stateModel
+          .find()
+          .populate([
+            {
+              path: "country",
+              model: "Country",
+            },
+          ])
+          .sort({ createdAt: -1 })
+          .skip(skip)
+          .limit(limit);
+      }
+      res.status(200).send({
+        success: true,
+        message: "States Retrieved Successfully",
+        states,
+        totalPages,
+        currentPage: page,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Error in fetching states",
+        error: error.message,
+      });
+    }
+  },
 
-            const states = await stateModel.find().populate([   
-                {
-                    'path':"country",
-                    'model':'Country'
-                },
-            ]).sort({createdAt:-1}).skip(skip).limit(limit);
-            res.status(200).send({
-                success: true,
-                message: "States Retrieved Successfully",
-                states,
-                totalPages,
-                currentPage: page
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({
-                success: false,
-                message: 'Error in fetching states',
-                error: error.message
-            });
-        }
-    },
 
-     // Method to create a new state
-     edit: async (req, res) => {
-        const { id,name,country } = req.body;
-        try {
-            const stateInfo = await stateModel.updateOne({_id:id},{ name,country });
-            res.status(200).send({
-                success: true,
-                message: "State Updated Successfully",
-                stateInfo
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({
-                success: false,
-                message: 'Error in creating state',
-                error: error.message
-            });
-        }
-    },
+  // Method to create a new state
+  edit: async (req, res) => {
+    const { id, name, country } = req.body;
+    try {
+      const stateInfo = await stateModel.updateOne(
+        { _id: id },
+        { name, country }
+      );
+      res.status(200).send({
+        success: true,
+        message: "State Updated Successfully",
+        stateInfo,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Error in creating state",
+        error: error.message,
+      });
+    }
+  },
 };
 
 // Export stateController
