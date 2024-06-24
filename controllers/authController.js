e82af55810932d3fcf11348abd28e67076c115c1
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-06-24 18:03:48 +0600

    emmail chnage

--- controllers/authController.js
+++ controllers/authController.js
@@ -100,8 +100,8 @@ const registerController = async (req, res) => {
       port: 465,
       secure: true, // Set to false for explicit TLS
       auth: {
-        user: "technovicinity.dev@gmail.com",
-        pass: "wsvrvojuwyraazog",
+        user: "turkscompass@gmail.com",
+        pass: "avrucxhaxvgdcjef",
       },
       tls: {
         // Do not fail on invalid certificates
@@ -451,8 +451,8 @@ const resetPasswordController = async (req, res) => {
     port: 465,
     secure: true, // Set to false for explicit TLS
     auth: {
-      user: "technovicinity.dev@gmail.com",
-      pass: "wsvrvojuwyraazog",
+      user: "turkscompass@gmail.com",
+      pass: "avrucxhaxvgdcjef",
     },
     tls: {
       // Do not fail on invalid certificates
