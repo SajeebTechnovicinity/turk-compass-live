5bbde12a6c06e8cd6c4251b6d7c3dad8816db15c
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-06-27 11:40:55 +0600

    sss

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -8,15 +8,15 @@ const packageController=(req,res)=>{
                     "package_benefit": [
                         {
                             "details":"Access to basic app features",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Users can explore essential functionalities of the Turk’s Compass app, such as browsing through the main categories of businesses and services without any cost."
                         },
                         {
                             "details":"View business listings and events",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Users can see various business listings, including restaurants, healthcare providers, and other services, as well as upcoming community events and promotions"
                         },
                         {
                             "details":"Ad-supported experience",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"This tier includes advertisements displayed throughout the app, which helps keep the basic features free for users"
                         }
 
                     ],
@@ -27,15 +27,15 @@ const packageController=(req,res)=>{
                     "package_benefit": [
                         {
                             "details":"Full access to job listings and applications",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Subscribers enjoy unrestricted access to all job listings and application features, enhancing their job search capabilities."
                         },
                         {
                             "details":"Ad-free experience",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"This tier eliminates advertisements, providing a smoother and more enjoyable user experience."
                         },
                         {
                             "details":"Access to Compass+ features including AI supported personalized advice, resume and cover letter building",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Subscribers benefit from advanced tools and personalized advice powered by AI, aiding in creating professional resumes and cover letters tailored for the Canadian job market."
                         }
 
                     ],
@@ -47,15 +47,15 @@ const packageController=(req,res)=>{
                     "package_benefit": [
                         {
                             "details":"Access to job listings and job application features",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Subscribers can view and apply for job opportunities listed in the app, providing a valuable resource for those seeking employment."
                         },
                         {
                             "details":"Ad-supported experience",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Despite the low monthly fee, this tier still includes advertisements, ensuring the subscription remains affordable while generating revenue."
                         },
                         {
                             "details":"No Compass+ access",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Users do not have access to premium features like AI-supported personalized advice or resume and cover letter building tools."
                         }
                     ]
                 },
@@ -65,24 +65,28 @@ const packageController=(req,res)=>{
                     "package_benefit": ["Full access to job listings and applications", "Ad-free experience", "Access to Compass+ features including AI supported personalized advice, resume and cover letter building"],
                     "package_benefit": [
                         {
-                            "details":"Verified Blue Tick status",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "details":"Full access to job listings and applications",
+                            "tooltip":"Employers enjoy unrestricted access to all application features in the Turk’s Compass app."
                         },
                         {
-                            "details":"Review job applications and CVs within the app",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "details":"Verified Blue Tick status",
+                            "tooltip":"Employers enjoy unrestricted access to all application features in the Turk’s Compass app."
                         },
+                        // {
+                        //     "details":"Review job applications and CVs within the app",
+                        //     "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                        // },
                         {
                             "details":"Custom questions for job applicants",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Employers can create tailored questions for applicants to answer, ensuring they receive the most relevant information."
                         },
                         {
                             "details":"1 banner ad per month",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"Employers are awarded one free banner ad per month to promote their business or job openings within the app."
                         },
                         {
                             "details":"Ad-free experience",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "tooltip":"This tier provides an ad-free experience for employers, allowing them to focus solely on managing job postings and applications without distractions."
                         }
                     ]
                 },
@@ -91,17 +95,21 @@ const packageController=(req,res)=>{
                     'description':{job_seeker_access:0,ads:0,compass:0,monthly_price:0,verify_employers:0,review_job_applications:0,custom_questions:0},
                     "package_benefit": [
                         {
-                            "details":"Verified Blue Tick status",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
+                            "details":"Access to basic app features",
+                            "tooltip":"Employers can explore essential functionalities of the Turk’s Compass app, such as browsing through the main categories of businesses and services without any cost."
                         },
                         {
-                            "details":"Receive job applications on email",
-                            "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
-                        },
+                            "details":"View business listings and events",
+                            "tooltip":"Employers can see various business listings, including restaurants, healthcare providers, and other services, as well as upcoming community events and promotions."
+                        },         
                         {
                             "details":"Ad-supported experience",
                             "tooltip":"In publishing and graphic design, Lorem ipsum is a placeholder text commonly"
-                        }
+                        },
+                        {
+                            "details":"No “Premium Employer” features",
+                            "tooltip":"The Basic Employer tier lacks premium features such as the Blue Tick status badge and the ability to ask custom questions to job applicants on the job portal."
+                        },
                     ]
     
                 },
